b7297cf7e7630963bce766ae1a63f49c8c59a880
AntonC9018 <anton.curm@gmail.com>
2024-03-10 23:23:03 +0200

    Trying to build on wsl

--- src/main.zig
+++ src/main.zig
@@ -2,9 +2,28 @@ const std = @import("std");
 const parser = @import("parser.zig");
 const pipelines = @import("pipelines.zig");
 
+const raylib = @import("raylib");
+
 pub fn main() !void
 {
-    try @import("pngDebug.zig").readTestFile();
+    // try @import("pngDebug.zig").readTestFile();
+
+    raylib.SetConfigFlags(raylib.ConfigFlags{ .FLAG_WINDOW_RESIZABLE = true });
+    raylib.InitWindow(800, 800, "hello world!");
+    raylib.SetTargetFPS(60);
+
+    defer raylib.CloseWindow();
+
+    while (!raylib.WindowShouldClose())
+    {
+        raylib.BeginDrawing();
+        defer raylib.EndDrawing();
+        
+        raylib.ClearBackground(raylib.BLACK);
+        raylib.DrawFPS(10, 10);
+
+        raylib.DrawText("hello world!", 100, 100, 20, raylib.YELLOW);
+    }
 }
 
 test
